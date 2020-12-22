e560ea2ed3c1ee40b722a028c9db355445c69df5
CindAlfi99 <acindi38@gmail.com>
2020-12-22 20:03:01 +0700

    update files

--- template/template.php
+++ template/template.php
@@ -27,7 +27,7 @@
           <a class="nav-link" href="listdistributor.php">Distributor</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="product/detail.php">Product</a>
+          <a class="nav-link" href="#">Product</a>
         </li>
         <li class="nav-item">
           <a class="nav-link" href="#">Keluar</a>
